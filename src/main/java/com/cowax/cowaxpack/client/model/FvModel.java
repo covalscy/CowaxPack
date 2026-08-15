adb6345a0108935aa870056462909db8b3e848b1
Covalscy <gartuyev20080512@mail.ru>
2026-08-16 01:22:46 +0300

    fix(1.20.1): extend VehicleModel and map custom bone names for turret/gun rotation

--- src/main/java/com/cowax/cowaxpack/client/model/FvModel.java
+++ src/main/java/com/cowax/cowaxpack/client/model/FvModel.java
@@ -4,14 +4,39 @@ import com.atsuishio.superbwarfare.client.model.entity.VehicleModel;
 import com.cowax.cowaxpack.CowaxPack;
 import com.cowax.cowaxpack.entity.FvEntity;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
 
 public class FvModel extends VehicleModel<FvEntity> {
-    // VehicleModel автоматически загружает модели, текстуры и LOD из конфигурации
-    // Файл: assets/cowaxpack/sbw/vehicles/fv.json
-    
+
+    @Override
+    public ResourceLocation getModelResource(FvEntity animatable) {
+        return new ResourceLocation(CowaxPack.MODID, "geo/fv.geo.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureResource(FvEntity animatable) {
+        return new ResourceLocation(CowaxPack.MODID, "textures/entity/fv.png");
+    }
+
     @Override
     public ResourceLocation getAnimationResource(FvEntity animatable) {
-        // VehicleModel не предоставляет анимации, нужно явно указать путь
         return new ResourceLocation(CowaxPack.MODID, "animations/fv.animation.json");
+    }
+
+    @Override
+    public @Nullable TransformContext<FvEntity> collectTransform(String boneName) {
+        if ("gun".equals(boneName) || "barrel".equals(boneName)) {
+            return (bone, vehicle, state) -> {
+                float pitch = vehicle.getTurretPitch(state.getPartialTick());
+                bone.setRotX(Mth.clamp(-pitch, vehicle.getTurretMinPitch(), vehicle.getTurretMaxPitch()) * Mth.DEG_TO_RAD);
+                CoreGeoBone barrelLaser = getAnimationProcessor().getBone("barrelLaser");
+                if (barrelLaser != null) {
+                    barrelLaser.setRotX(bone.getRotX());
+                }
+            };
+        }
+        return super.collectTransform(boneName);
     }
 }
--- src/main/java/com/cowax/cowaxpack/client/model/Zenit_2C6Model.java
+++ src/main/java/com/cowax/cowaxpack/client/model/Zenit_2C6Model.java
@@ -1,11 +1,15 @@
 package com.cowax.cowaxpack.client.model;
 
+import com.atsuishio.superbwarfare.client.model.entity.VehicleModel;
 import com.cowax.cowaxpack.CowaxPack;
 import com.cowax.cowaxpack.entity.Zenit_2C6Entity;
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib.model.GeoModel;
+import net.minecraft.util.Mth;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
 
-public class Zenit_2C6Model extends GeoModel<Zenit_2C6Entity> {
+public class Zenit_2C6Model extends VehicleModel<Zenit_2C6Entity> {
+
     @Override
     public ResourceLocation getModelResource(Zenit_2C6Entity animatable) {
         return new ResourceLocation(CowaxPack.MODID, "geo/2c6.geo.json");
@@ -19,5 +23,43 @@ public class Zenit_2C6Model extends GeoModel<Zenit_2C6Entity> {
     @Override
     public ResourceLocation getAnimationResource(Zenit_2C6Entity animatable) {
         return new ResourceLocation(CowaxPack.MODID, "animations/2c6.animation.json");
+    }
+
+    @Override
+    public @Nullable TransformContext<Zenit_2C6Entity> collectTransform(String boneName) {
+        if ("cannon".equals(boneName) || "turret".equals(boneName)) {
+            return (bone, vehicle, state) -> {
+                float yaw = vehicle.getTurretYaw(state.getPartialTick());
+                bone.setRotY(yaw * Mth.DEG_TO_RAD);
+                CoreGeoBone turretLaser = getAnimationProcessor().getBone("turretLaser");
+                if (turretLaser != null) {
+                    turretLaser.setRotY(bone.getRotY());
+                }
+                bone.setHidden(vehicle.isWreck() && vehicle.hasTurret() && vehicle.getSympatheticDetonated());
+            };
+        }
+        if ("barrel".equals(boneName)) {
+            return (bone, vehicle, state) -> {
+                float pitch = vehicle.getTurretPitch(state.getPartialTick());
+                bone.setRotX(Mth.clamp(-pitch, vehicle.getTurretMinPitch(), vehicle.getTurretMaxPitch()) * Mth.DEG_TO_RAD);
+                CoreGeoBone barrelLaser = getAnimationProcessor().getBone("barrelLaser");
+                if (barrelLaser != null) {
+                    barrelLaser.setRotX(bone.getRotX());
+                }
+            };
+        }
+        if ("aim".equals(boneName)) {
+            return (bone, vehicle, state) -> {
+                float aimRot = vehicle.getGunYRot(state.getPartialTick());
+                float turretYaw = vehicle.getTurretYaw(state.getPartialTick());
+                bone.setRotY((aimRot - turretYaw) * Mth.DEG_TO_RAD);
+            };
+        }
+        if ("aim_cannon".equals(boneName)) {
+            return (bone, vehicle, state) -> {
+                bone.setRotX(-vehicle.getGunXRot(state.getPartialTick()) * Mth.DEG_TO_RAD);
+            };
+        }
+        return super.collectTransform(boneName);
     }
 }
