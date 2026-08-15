122c48d81ab90843e8a6117dc253056e71407cb0
Covalscy <gartuyev20080512@mail.ru>
2026-08-15 20:24:37 +0300

    chore: remove debug logging from FvEntity and Zenit_2C6Entity

--- src/main/java/com/cowax/cowaxpack/entity/Zenit_2C6Entity.java
+++ src/main/java/com/cowax/cowaxpack/entity/Zenit_2C6Entity.java
@@ -2,15 +2,8 @@ package com.cowax.cowaxpack.entity;
 
 import com.atsuishio.superbwarfare.entity.vehicle.base.GeoVehicleEntity;
 import com.atsuishio.superbwarfare.entity.vehicle.damage.DamageModifier;
-import com.mojang.logging.LogUtils;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import org.slf4j.Logger;
 import software.bernie.geckolib.animation.AnimatableManager;
 import software.bernie.geckolib.animation.AnimationController;
 import software.bernie.geckolib.animation.AnimationState;
@@ -18,46 +11,9 @@ import software.bernie.geckolib.animation.PlayState;
 import software.bernie.geckolib.animation.RawAnimation;
 
 public class Zenit_2C6Entity extends GeoVehicleEntity {
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public Zenit_2C6Entity(EntityType<? extends GeoVehicleEntity> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
-    }
-
-    @Override
-    public InteractionResult interact(Player player, InteractionHand hand) {
-        int seatsCount = computed().seats().size();
-        int maxPass = getMaxPassengers();
-        int curPass = getPassengers().size();
-        boolean isClient = level().isClientSide();
-
-        String debugMsg = String.format("[2C6 DEBUG interact] Side: %s, maxPass: %d, seats: %d, curPass: %d, shift: %b, canAdd: %b",
-                isClient ? "CLIENT" : "SERVER", maxPass, seatsCount, curPass, player.isShiftKeyDown(), canAddPassenger(player));
-
-        LOGGER.info(debugMsg);
-        player.displayClientMessage(Component.literal(debugMsg), false);
-
-        InteractionResult result = super.interact(player, hand);
-
-        String resultMsg = String.format("[2C6 DEBUG result] Side: %s -> %s, playerVehicle: %s",
-                isClient ? "CLIENT" : "SERVER", result, player.getVehicle() != null ? player.getVehicle().getName().getString() : "null");
-        LOGGER.info(resultMsg);
-        player.displayClientMessage(Component.literal(resultMsg), false);
-
-        return result;
-    }
-
-    @Override
-    protected boolean canAddPassenger(Entity pPassenger) {
-        boolean can = super.canAddPassenger(pPassenger);
-        LOGGER.info("[2C6 DEBUG canAddPassenger] passenger: {}, result: {}", pPassenger.getName().getString(), can);
-        return can;
-    }
-
-    @Override
-    protected void addPassenger(Entity pPassenger) {
-        LOGGER.info("[2C6 DEBUG addPassenger] adding: {}", pPassenger.getName().getString());
-        super.addPassenger(pPassenger);
     }
 
     @Override
